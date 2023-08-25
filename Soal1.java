8467323e05915ec6207764d56c32f392eeb0687f
fikrihairul <117856876+fikrihairul@users.noreply.github.com>
2023-08-25 23:05:42 +0700

    Add files via upload

---
+++ Soal1.java
@@ -0,0 +1,13 @@
+package com.mycompany.welcome;
+
+/**
+ * @author Fikri
+ */
+public class Soal1 {
+    public static void main(String[] args) {
+       byte angka1 = 125;
+       byte angka2 = 6;
+       byte hasil = (byte) (angka1 + angka2);
+       System.out.println(+hasil);
+   }
+}
